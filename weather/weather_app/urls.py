56b5abeaeaefe95f635e6c8a41e459ec580289a7
Gunjan Bholane <152423360+GunjanBholane@users.noreply.github.com>
2025-07-03 00:43:00 +0530

    Update urls.py

--- weather/weather_app/urls.py
+++ weather/weather_app/urls.py
@@ -5,4 +5,6 @@ from weather_app.views import UserRegistrationView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',UserRegistrationView.as_view(),name='register'),
+    path('weather/', WeatherView.as_view(), name='weather'),
+
 ]
